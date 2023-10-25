14f362c8262de1163840ad591a00c6e61d784502
YokoyamaTaichi <yokoyamataichi@YokoyamaTaichinoMacBook-Air-2.local>
2023-10-25 19:03:36 +0900

    navbarのレイアウトを調整

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
-import { Button, buttonVariants } from "../ui/button";
 import Image from "next/image";
 import { SelectGeneration } from "../organisms/select-generation";
 
 export const Header = () => {
   return (
     <nav className="bg-white w-full border-b md:border-0">
-      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+      <div className="flex items-center justify-between p-2 max-w-screen-xl mx-auto md:px-8">
+        <div className="flex-shrink-0">
           <Link href="/">
             <Image src="/logo.png" height={40} width={150} alt="ロゴ" />
           </Link>
         </div>
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+        <div className="ml-auto">
           <SelectGeneration />
         </div>
       </div>
