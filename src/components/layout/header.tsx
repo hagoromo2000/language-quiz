df5fff921a41fd043fa762fb5abc0c8fc72b03e8
YokoyamaTaichi <yokoyamataichi@YokoyamaTaichinoMacBook-Air-2.local>
2023-10-25 16:37:36 +0900

    ロゴを透過

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import { Button, buttonVariants } from "../ui/button";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <header>
       <Button asChild size="lg" variant="outline">
-        <Link href="/">ポケモン中国語クイズ</Link>
+        <Link href="/">
+          <Image src="/icon.png" height={50} width={50} alt="ロゴ" />
+          ポケモン中国語クイズ
+        </Link>
       </Button>
     </header>
   );
