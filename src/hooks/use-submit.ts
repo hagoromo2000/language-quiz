69074b2d865191ed5fe3823a55af075e0349f7f1
hagoromo2000 <revtm000126@gmail.com>
2023-10-29 18:48:49 +0900

    コメントアウトを追加

--- src/hooks/use-submit.ts
+++ src/hooks/use-submit.ts
@@ -13,7 +13,7 @@ export const hiraganaToKatakana = (str: string): string => {
 
 export const pokemonSchema = z.string().refine(
   (value) => {
-    const regex = /^[ぁ-んァ-ヶー\u2640\u2642]+$/;
+    const regex = /^[ぁ-んァ-ヶー\u2640\u2642]+$/; // ニドラン♂, ニドラン♀を許容するため、\u2640, \u2642を追加
     return regex.test(value) && value.length <= 6;
   },
   {
